51d7c41b007bf0707799c11f76c1e512c0470f9d
linkcheng <zhenglong1992@126.com>
2020-03-30 16:48:32 +0800

    add rabbitmq-server configuration

---
+++ springboot-stream/src/test/java/cn/xyf/SpringbootStreamApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.xyf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootStreamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
